52798489ecd5b78d40797c5a518a4c1bbdacca18
LEON <dlwotjr2@gmail.com>
2021-11-06 12:38:45 +0900

    단방향 페이지로 개편

--- public/index.js
+++ public/index.js
@@ -1,6 +1,12 @@
+const heroBtn = document.querySelector(".heroBtn");
 const infoBtn = document.querySelector(".infoBtn");
+
+heroBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector(".hero__img").scrollIntoView(true);
+})
 
 infoBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    document.querySelector(".hero").scrollIntoView(true);
+    document.querySelector(".hero__info").scrollIntoView(true);
 })
