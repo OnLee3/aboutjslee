b3c6ad233697aef8b04e10888663d83a93ae4591
LEON <dlwotjr2@gmail.com>
2021-11-06 13:03:04 +0900

    전체 레이아웃 개편, nav 클릭시 해당 콘텐츠로 이동기능 추가

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 const heroBtn = document.querySelector(".heroBtn");
 const infoBtn = document.querySelector(".infoBtn");
+const workBtn = document.querySelector(".workBtn");
+const contactBtn = document.querySelector(".contactBtn");
 
 heroBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -9,4 +11,14 @@ heroBtn.addEventListener("click", (e) => {
 infoBtn.addEventListener("click", (e) => {
     e.preventDefault();
     document.querySelector(".hero__info").scrollIntoView(true);
+})
+
+workBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector(".hero__work").scrollIntoView(true);
+})
+
+contactBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.querySelector(".contact").scrollIntoView(true);
 })
