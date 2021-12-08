fc34d0cfe8d390d3c4d1fcd67e206da626401d3c
LEON <dlwotjr2@gmail.com>
2021-12-08 23:03:16 +0900

    콘솔텍스트 출력, 언더바 구현

--- public/index.js
+++ public/index.js
@@ -26,50 +26,53 @@ contactBtn.addEventListener("click", (e) => {
 })
 
 // 콘솔이펙트 타이틀
-consoleText(['안녕하세요.', '이재석 입니다.', '잘 부탁드립니다 :)'], 'console__text', ['tomato','rebeccapurple','lightblue'])
+consoleText(['안녕하세요', '이재석 입니다.', '잘 부탁드립니다 :)'], 'text',['#111','#111', '#111']);
 
 function consoleText(words, id, colors) {
-    if (colors === undefined) colors = ['#fff'];
-    let visible = true;
-    let console = document.getElementById('console');
-    let letterCount = 1;
-    let x = 1;
-    let waiting = false;
-    let target = document.getElementById(id);
-    target.setAttribute('style', 'color:' + colors[0])
-    window.setInterval(() => {
-        if (letterCount === 0 && waiting === false){
-            waiting === true;
-            target.innerHTML = words[0].substring(0, letterCount);
-            window.setTimeout(() => {
-                let usedColor = colors.shift();
-                colors.push(usedColor);
-                let usedWord = words.shift();
-                words.push(usedWord);
-                x = 1;
-                target.setAttribute('style', 'color' + colors[0]);
-                letterCount += x;
-                waiting = false;
-            }, 1000)
-        } else if (letterCount === words[0].length + 1 && waiting === false){
-            waiting = true;
-            window.setTimeout(() => {
-                x = -1;
-                letterCount += x;
-                waiting = false;
-            }, 1000)
-        } else if (waiting === false) {
-                target.innerHTML = words[0].substring(0, letterCount)
-                letterCount += x;
-        }
-    }, 120)
-    window.setInterval(() => {
-        if (visible === true){
-            console.className = 'console__underscore hidden'
-            visible = false;
-        } else {
-            console.className = 'console__underscore'
-            visible = true;
-        }
-    }, 400)
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
 }
